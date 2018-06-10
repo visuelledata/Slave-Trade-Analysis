09335db490a2a286fe86357b610a0d8ce56eb4fa
Chris Peralta <chris99994@gmail.com>
2018-06-10 18:10:43 +0800

    is.output.same() now has support for most 'dplyr syntax' functions, add_quo() formats args for invoke_map()

---
+++ functions/add_quo.R
@@ -0,0 +1,44 @@
+require(purrr)
+require(magrittr)
+require(rlang)
+
+#param ... is any list of arguments
+
+add_quo <- function(...) {
+  ## Formats arguments for use in invoke_map()
+  
+  # Create a logical vector for use as an index below
+  index <- exprs(...) %>%                       # Captures the arguments
+    unlist() %>%                                # Makes them mappable
+    map_if(negate(is.name), 
+           .f = function(x) {return(TRUE)}) %>% # Replaces unnecessary args with TRUE
+    map_if(is.name, as_character) %>%           # Converts named arguments to characters
+    map_if(is.character, exists) %>%            # Replaces named args that aren't objects as FALSE
+    unlist()                                    # To make index mappable
+  
+  # Wraps named args that aren't objects in new_quosure()
+  exprs(...) %>%                    # Captures arguments
+    unlist() %>%                    # Makes them mappable
+    map_if(!index, new_quosure,     # Wraps non-object named arg in new_quosure()
+           env = global_env()) %>%  # new_quosure() used instead of quo() as env must be global
+    map_if(index, eval) %>%         # Removes expr() formatting
+    list()                          # Makes arguments usable in invoke_map()
+}
+
+
+
+# dotdotdot <- function(...) return(list(list(...))) # ... args are passed into invoke_map() like this
+# 
+# dotdotdot(data = iris, min = 5, x = 1:4, g = quo(aaa), mtcars, quo(neio))
+# add_quo(data = iris, min = 5, x = 1:4, g = aaa, mtcars, neio)
+# is.output.same(list(add_quo, dotdotdot))
+# 
+# a <- add_quo(data = iris, min = 5, x = 1:4, aesrtion) %>% # Can't use is.output.same as args are different
+#   unlist() %>%
+#   map(class)
+# b <- dotdotdot(data = iris, min = 5, x = 1:4, g = quo(aesrtion)) %>%
+#   unlist() %>%
+#   map(class)
+# identical(a,b)
+
+
--- functions/is.output.same.R
+++ functions/is.output.same.R
@@ -1,19 +1,62 @@
+# To do: add support for mutate(iris, col1 + col2) style functions
+
 require(purrr)
+require(magrittr) 
+require(rlang) # add_quo uses this package
+source('functions/add_quo.R') # Helper function that wraps certain arguments in quo()
 
 #param function_list is a list of functions
 #param ... are the arguments for those functions
 
-is.output.same <- function(function_list, ...) {
+is.output.same<- function(.f_list, ...) {
   ## Checks to see if functions produce identical output
+  
+  param_list <- add_quo(...) # Enables function with dplyr syntax to work
+  
   # Error checking
-  if(!all(map_lgl(function_list, is.function)))
+  if(!all(map_lgl(.f_list, is.function)))
     stop('Element in function_list is not a function.')
-  if(length(list(...)) == 0) 
-    stop('Example call: is.output.same(list(cumsum, cumsum), x = c(1,2,3)) 
-          Argument must be given. See example above.')
-  if(identical(reduce(map(function_list, formalArgs), intersect), character(0))) 
+  if(length(param_list) == 0)
+    stop('Example call: is.output.same(list(cumsum, cumsum), x = c(1,2,3))
+         Argument must be given. See example above.')
+  if(identical(reduce(map(.f_list, formalArgs), intersect), character(0))) 
     stop('Functions must have a common argument.') 
   
-  output <- invoke_map(.f = function_list, .x = list(list(...))) # Pass args to functions
-  reduce(output, identical) # Sees if all outputs are identical
+  invoke_map(.f = .f_list, .x = param_list) %>%  # Pass args to functions
+    reduce(identical) # Sees if all outputs are identical
 }
+
+
+
+# ctrl + shift + c while all code is highlighted to test is.output.same
+
+# m <- function(data, col_name) {
+#   col_name <- enquo(col_name)
+#   select(data, !!col_name)
+# }
+# n <- m
+# 
+# head(iris)
+# 
+# # Works with 'dplyr syntax' functions
+# m(data = iris, col_name = Species)
+# is.output.same(list(m, n), data = iris, col_name = Species)
+#   #[1] TRUE
+# 
+# # Works with unnamed arguments
+# select(.data = iris, Species, Sepal.Width)
+# is.output.same(list(select, select), .data = iris, Species, Sepal.Width)
+#   #[1] TRUE
+# 
+# # Does not work with 'dplyr syntax' functions that perform on operation such as the one in mutate
+# mutate(.data = iris, sum_numeric = Sepal.Length + Sepal.Width)
+# is.output.same(list(mutate, mutate), .data = iris, sum_numeric = Sepal.Length + Sepal.Width)
+#   #[1] TRUE
+# 
+# #Supports unnamed arguments
+# is.output.same(list(cumsum, cumsum), 1:4)
+#   #[1] TRUE
+# is.output.same(list(cumprod, cumsum), 1:4)
+#   #[1] FALSE
+# is.output.same(list(select, select), iris, Species, Sepal.Width)
+#   #[1] TRUE
