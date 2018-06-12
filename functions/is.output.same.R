3279b148acbcc89542fc891fadad299083dede1f
Chris Peralta <chris99994@gmail.com>
2018-06-13 01:24:39 +0800

    Created a quote_pipe(), using magrittr pipe. Updated is.output.same()

--- functions/is.output.same.R
+++ functions/is.output.same.R
@@ -9,33 +9,36 @@ require(magrittr)
 require(purrr)
 
 is.output.same <- function(.call, ...) {
-
-  #Error checking
-  .call # To make sure the call is valid before proceeding
-  if (!all(map_lgl(list(...), is.function))) stop('An optional argument is not a function.')
   
-  call <- substitute(.call)               # Captures function call
+  if(!is.call(.call)) .call <- substitute(.call)
+  
+  #Error checking
+  eval(.call) # To make sure the call is valid before proceeding
+  if (!all(map_lgl(list(...), is.function))) stop('An optional argument is not a function.')
+  if (missing(...)) stop('Need at least 1 optional argument.')
+  
   f_names <- eval(substitute(alist(...))) # Makes list of f_names
   
   
-  map2(rep(list(call), length(f_names)),  # Creates list of new function calls
+  map2(rep(list(.call), length(f_names)),  # Creates list of new function calls
        f_names,
        function(.x, .y, i) {.x[[1]] <- .y; return(.x)} 
     ) %>%
     map(eval) %>%                         # Evaluates function calls
-    map_lgl(identical, x = .call) %>%     # Checks output of new calls against output of original call 
+    map_lgl(identical, x = eval(.call)) %>%     # Checks output of new calls against output of original call 
     all()                                 # Returns TRUE if calls produce identical outputs
 }
 
 
 ## Examples
 
-# is.out.same(map(1:3, cumsum), lapply) #TRUE
-# is.out.same(cumsum(1:3), cumprod) #FALSE
-# is.out.same(sum(1:3), prod, iris) # Intended error
-# is.out.same(subset(iris, Sepal.Width < 5), filter) #TRUE
-# is.out.same(sum(1:3), prod) #Why is the output wrong?
+is.output.same(map(1:3, cumsum), lapply) #TRUE
+is.output.same(cumsum(1:3), cumprod) #FALSE
+is.output.same(sum(1:3), prod, iris) # Intended error
+is.output.same(subset(iris, Sepal.Width < 5), filter) #TRUE
+is.output.same(sum(1:3), prod) #Why is the output wrong?
+map(1:3, cumsum) %''%    #TRUE
+  is.output.same(lapply)
+is.output.same(sum(1:3), prod, iris) # Intended error
+is.output.same(sum(1:3)) # Intended error
 
-# Doesn't work unfortunately 
-# map(1:3, cumsum) %>% 
-#   is.out.same(cumprod)
